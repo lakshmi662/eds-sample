a0d8747291ac796cb77ffdec9b37c0a931788ed8
Lakshmi891 <lmamidi@adobe.com>
2025-02-12 16:01:02 +0530

    added css code

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,10 +1,16 @@
-function embedGoogleDriveVideo(link) {
-    const embedLink = link;
+function displayGoogleDriveVideo(driveUrl) {
+    const embedUrl = driveUrl;
     const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', embedLink);
-    const videoDiv = document.querySelector('.video');
-    videoDiv.innerHTML = '';
-    videoDiv.appendChild(iframe);
+    iframe.setAttribute('src', embedUrl);
+    iframe.setAttribute('class', 'video-iframe'); 
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'autoplay; encrypted-media');
+    iframe.setAttribute('allowfullscreen', true);
+
+    const videoContainer = document.querySelector('.video');
+    videoContainer.innerHTML = '';  
+    videoContainer.appendChild(iframe); 
 }
-const driveLink = 'https://drive.google.com/file/d/1HLxrLuB3oYJHaZS04ojYj3H5-dO5LcTM/preview';
-embedGoogleDriveVideo(driveLink);
+
+const googleDriveVideoUrl = 'https://drive.google.com/file/d/1HLxrLuB3oYJHaZS04ojYj3H5-dO5LcTM/preview';
+displayGoogleDriveVideo(googleDriveVideoUrl);
