d657725291c164662d75007c62a8e68494c2914c
Lakshmi891 <lmamidi@adobe.com>
2025-02-12 14:06:18 +0530

    added place

--- blocks/placeholder/placeholder.js
+++ blocks/placeholder/placeholder.js
@@ -1,3 +1,5 @@
-import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; export default async function decorate(block) { const placeholders = await fetchPlaceholders(''); 
-    console.log(placeholder);
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
+export default async function decorate(block) { 
+const placeholders = await fetchPlaceholders(''); 
+console.log(placeholder);
 }
